e424ca7797e23bb6170898962ea88134f5cfbb4d
KILLA <killa@MBP-de-KILLA.localdomain>
2024-03-18 15:06:28 -0400

    adding header en .c file

---
+++ _printf.c
@@ -0,0 +1,34 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/*
+ * Custom printf function that handles conversion specifiers %d and %i
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+
+	int count = 0;
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'd' || *format == 'i')
+			{
+				int num = va_arg(args, int);
+				count += printf("%d", num);
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (count);
