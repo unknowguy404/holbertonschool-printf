ea897f3481de3e35a276c6b22db8e99be3383691
Thaigerhoo <jhhonathhan42@gmail.com>
2024-03-19 21:36:50 -0400

    Last fix

--- _printf.c
+++ _printf.c
@@ -1,87 +1,67 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 int _printf(const char *format, ...)
 {
-	va_list args;
-	unsigned int count = 0;
+    va_list args;
+    unsigned int count = 0;
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'c')
-			{
-				int c = va_arg(args, int);
-				putchar(c);
-				count++;
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(args, char *);
-				if (str == NULL)
-				{
-					count += printf("(null)");
-				}
-				else
-				{
-					while (*str)
-					{
-						putchar(*str);
-						str++;
-						count++;
-					}
-				}
-			}
-			else if (*format == '%')
-			{
-				putchar('%');
-				count++;
-			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				int num = va_arg(args, int);
-				count += printf("%d", num);
-			}
-			else if (*format == 'o')
-			{
-				unsigned int octal_num = va_arg(args, int);
-				count += printf("%o", octal_num);
-			}
-			else if (*format == 'p')
-			{
-				void *ptr = va_arg(args, void *);
-				count += printf("%p", ptr);
-			}
-			else if (*format == 'x' || *format == 'X')
-			{
-				unsigned int hex_num = va_arg(args, int);
-				count += printf("%x", hex_num);
-			}
-			else if (*format == 'u')
-			{
-				unsigned int unsigned_num = va_arg(args, int);
-				count += printf("%u", unsigned_num);
-			}
-			else
-			{
-				putchar('%');
-				putchar(*format);
-				count++;
-			}
-		}
-		else
-		{
-			putchar(*format);
-			count++;
-		}
-		format++;
-	}
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == '\0')
+            {
+                putchar('%');
+                count++;
+                break;
+            }
+            else if (*format == 'c')
+            {
+                int c = va_arg(args, int);
+                putchar(c);
+                count++;
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char *);
+                if (str == NULL)
+                {
+                    count += printf("(null)");
+                }
+                else
+                {
+                    while (*str)
+                    {
+                        putchar(*str);
+                        str++;
+                        count++;
+                    }
+                }
+            }
+            else if (*format == '%')
+            {
+                putchar('%');
+                count++;
+            }
+            else
+            {
+                putchar('%');
+                putchar(*format);
+                count += 2;
+            }
+        }
+        else
+        {
+            putchar(*format);
+            count++;
+        }
+        format++;
+    }
 
-	va_end(args);
-	return count;
+    va_end(args);
+    return count;
 }
+
