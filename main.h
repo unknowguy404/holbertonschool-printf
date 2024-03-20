4522526dec8389d7cf244dba45c41b4b2f1924e5
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-03-19 23:55:32 -0400

    I forgot to upload main.h

--- main.h
+++ main.h
@@ -13,5 +13,13 @@
 #include <sys/time.h>
 
 int _printf(const char *format, ...);
-
+int format_args(const char *format, va_list args);
+int process_format(char format, va_list args);
+int process_char(va_list args);
+int process_string(va_list args);
+int process_decimal(va_list args);
+int process_octal(va_list args);
+int process_pointer(va_list args);
+int process_hexadecimal(va_list args);
+int process_unsigned(va_list args);
 #endif /** MAIN_H*/
