c643a3f0a1b030db9e7deb2107cf1705224e3f88
Thaigerhoo <jhhonathhan42@gmail.com>
2024-03-19 20:40:32 -0400

    Restoring project

---
+++ _printf.c
@@ -0,0 +1,91 @@
+#include "main.h"
+/**
+ *
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	unsigned int count = 0;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+			{
+				break;
+			}
+			if (*format == 'c')
+			{
+				int c = va_arg(args, int);
+				putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
+				if (str == NULL)
+				{
+					count += printf("(null)");
+				}
+				else
+				{
+					while (*str)
+					{
+						putchar(*str);
+						str++;
+						count++;
+					}
+				}
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				count++;
+			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				int num = va_arg(args, int);
+				count += printf("%d", num);
+			}
+			else if (*format == 'o')
+			{
+				unsigned int octal_num = va_arg(args, int);
+				count += printf("%o", octal_num);
+			}
+			else if (*format == 'p')
+			{
+				void *ptr = va_arg(args, void *);
+				count += printf("%p", ptr);
+			}
+			else if (*format == 'x' || *format == 'X')
+			{
+				unsigned int hex_num = va_arg(args, int);
+				count += printf("%x", hex_num);
+			}
+			else if (*format == 'u')
+			{
+				unsigned int unsigned_num = va_arg(args, int);
+				count += printf("%u", unsigned_num);
+			}
+			else
+			{
+				putchar('%');
+				count++;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+
+	va_end(args);
+	return count;
+}
