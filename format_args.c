a78e8d95b1cdffed66e7b0d50e86d950a4c9063e
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-03-19 23:54:48 -0400

    It didnt work for me to create a single function within printf, I divided it into four parts for Betty

---
+++ format_args.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+int format_args(const char *format, va_list args)
+{
+	int count = 0;
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+			{
+				putchar('%');
+				count++;
+				break;
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				count++;
+			}
+			else
+			{
+				count += process_format(*format, args);
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+
+	return (count);
+}
---
+++ process_format.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+int process_format(char format, va_list args)
+{
+	int count = 0;
+
+	if (format == 'c')
+	{
+		count += process_char(args);
+	}
+	else if (format == 's')
+	{
+		count += process_string(args);
+	}
+	else if (format == 'd' || format == 'i')
+	{
+		count += process_decimal(args);
+	}
+	else if (format == 'o')
+	{
+		count += process_octal(args);
+	}
+	else if (format == 'p')
+	{
+		count += process_pointer(args);
+	}
+	else if (format == 'x' || format == 'X')
+	{
+		count += process_hexadecimal(args);
+	}
+	else if (format == 'u')
+	{
+		count += process_unsigned(args);
+	}
+	else
+	{
+		putchar('%');
+		putchar(format);
+		count += 2;
+	}
+
+	return count;
+}
