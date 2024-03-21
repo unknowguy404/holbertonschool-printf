371db974c5ed2b2d0a9cef8db67b1d0076f9855a
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-03-21 12:27:40 -0400

    new details

--- handle_conversion.c
+++ handle_conversion.c
@@ -1,43 +1,32 @@
 #include "main.h"
 int handle_conversion(char specifier, va_list args)
 {
-	unsigned int printed_chars = 0;
+	int printed_chars = 0;
 
 	switch (specifier)
 	{
 	case 'c':
-		putchar(va_arg(args, int));
+	{
+		char c = va_arg(args, int);
+		putchar(c);
 		printed_chars++;
 		break;
+	}
 	case 's':
-		printed_chars += print_string(va_arg(args, char *));
+	{
+		char *s = va_arg(args, char *);
+		printed_chars += print_string(s);
 		break;
+	}
 	case '%':
+	{
 		putchar('%');
 		printed_chars++;
 		break;
-	case 'd':
-	case 'i':
-		printed_chars += process_decimal(args);
-		break;
-	case 'o':
-		printed_chars += process_octal(args);
-		break;
-	case 'p':
-		printed_chars += process_pointer(args);
-		break;
-	case 'x':
-		printed_chars += process_hexadecimal(args);
-		break;
-	case 'u':
-		printed_chars += process_unsigned(args);
-		break;
+	}
 	default:
-		putchar('%');
-		putchar(specifier);
-		printed_chars += 2;
 		break;
 	}
 
-	return (printed_chars);
+	return printed_chars;
 }
