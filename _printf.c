14ac33210077dc14f2f4f7c97b956a719578cf4f
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-03-21 02:02:03 -0400

    update

--- _printf.c
+++ _printf.c
@@ -2,26 +2,12 @@
 
 int _printf(const char *format, ...)
 {
-	const char *ptr;
 	int printed_chars = 0;
 	va_list args;
 
 	va_start(args, format);
-
-	for (ptr = format; *ptr != '\0'; ptr++)
-	{
-		if (*ptr == '%')
-		{
-			putchar('%');
-			printed_chars++;
-		}
-		else
-		{
-			putchar(*ptr);
-			printed_chars++;
-		}
-	}
-
+	printed_chars = parse_format(format, args);
 	va_end(args);
-	return printed_chars;
+
+	return (printed_chars);
 }
